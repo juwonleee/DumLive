d5f833804470a4fcc9ea3b1cdff7ddd3f43230b9
HansolWorld <hs9269@gmail.com>
2023-06-08 16:12:09 +0900

    add accessibility

--- Scrumdinger/Scrumdinger/Views/CardView.swift
+++ Scrumdinger/Scrumdinger/Views/CardView.swift
@@ -14,14 +14,17 @@ struct CardView: View {
         VStack(alignment: .leading) {
             Text("\(dailyScrum.title)")
                 .font(.headline)
+                .accessibilityAddTraits(.isSelected)
             Spacer()
             HStack {
                 Label("\(dailyScrum.attendees.count)", systemImage: "person.3")
                     .font(.caption)
+                    .accessibilityLabel("\(dailyScrum.attendees.count) attendees")
                 Spacer()
                 Label("\(dailyScrum.lengthInMinutes)", systemImage: "clock")
                     .font(.caption)
                     .labelStyle(.trailingIcon)
+                    .accessibilityLabel("\(dailyScrum.lengthInMinutes)  minute meeting")
             }
         }
         .foregroundColor(dailyScrum.theme.accentColor)
