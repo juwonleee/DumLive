4cbc9e672977c9f1a3f7aaf541d2f0c595844a9c
juwonleee <juwonlee0509@gmail.com>
2023-06-08 15:06:55 +0900

    Feat: Add First Exercise File

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Lee Juwon on 2023/06/08.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
