fd1eb248f7a50d3214c3612f688d64d7110747ee
HansolWorld <hs9269@gmail.com>
2023-06-08 15:23:54 +0900

    add Theme model

--- Scrumdinger/Scrumdinger/Models/Theme.swift
+++ Scrumdinger/Scrumdinger/Models/Theme.swift
@@ -5,4 +5,23 @@
 //  Created by apple on 2023/06/08.
 //
 
-import Foundation
+import SwiftUI
+
+enum Theme: String {
+    case yellow
+    case blue
+    case red
+    
+    var accentColor: Color {
+        switch self {
+        case .yellow:
+            return .black
+        case .blue, .red:
+            return .white
+        }
+    }
+    
+    var mainColor: Color {
+        Color(rawValue)
+    }
+}
