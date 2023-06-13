57e6637de5e17fe5f1ab36909fa64cacd7c5f919
jis00ya <wltngnb@gmail.com>
2023-06-13 19:22:18 +0900

    [Views] Displaying data in a list(#2)

--- Scrumdinger/Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Scrumdinger/Models/DailyScrum.swift
@@ -7,11 +7,24 @@
 
 import Foundation
 
-struct DailyScrum {
+struct DailyScrum: Identifiable {
+    
+    let id: UUID //hashable protocol 채택
+    
     let title: String
     let attendees: [String]
     let lengthInMinutes: Int
     let theme: Theme
+    
+    //id: UUID = UUID()를 넣기 위해
+
+    init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
+        self.id = id
+        self.title = title
+        self.attendees = attendees
+        self.lengthInMinutes = lengthInMinutes
+        self.theme = theme
+    }
 }
 
 extension DailyScrum {
@@ -20,4 +33,5 @@ extension DailyScrum {
         DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, theme: .blue),
         DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 5, theme: .red)
     ]
+    
 }
