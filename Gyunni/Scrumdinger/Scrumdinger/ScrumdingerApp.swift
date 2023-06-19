32e24bae0dbedb70d13ec34b147fced0d4f55b89
seunggyun-jeong <jskyun98@gmail.com>
2023-06-19 15:30:24 +0900

    [ADD] Create new project

---
+++ Gyunni/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by 정승균 on 2023/06/19.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
